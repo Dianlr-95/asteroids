bd04eceb739753db2ac5203f9cec3941c4650eed
Dianlr-95 <dgalr957@gmail.com>
2025-06-14 20:33:23 +0200

    Draw player

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = None
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Start game loop---------------------
 
     while True:
         for event in pygame.event.get():
@@ -13,8 +17,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+
+    # End game loop-----------------------
 
 if __name__ == "__main__":
     main()
