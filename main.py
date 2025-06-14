755cb527dc51ed1e4cce5a8d156f45695c28b51e
Dianlr-95 <dgalr957@gmail.com>
2025-06-14 20:21:38 +0200

    Limit to 60 fps and add circleshape.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = None
+    clock = pygame.time.Clock()
+
     while True:
-        pygame.init()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill(color="black")
-        pygame.display.flip()
-        print("Starting Asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
 
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
